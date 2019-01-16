fae38e026eb9b22b02231565819645f123d251dd
whq <dawangong@126.com>
2019-01-16 18:05:49 +0800

    update

--- react-demo-build/src/components/header.js
+++ react-demo-build/src/components/header.js
@@ -4,14 +4,13 @@ import './header.css';
 export default class Header extends Component {
     constructor(props) {
         super(props);
+        this.title = '我是header';
     }
 
     render() {
         return (
            <div className="header">
-               <p>
-                   我是header
-               </p>
+               <p>{this.title}</p>
            </div>
         );
     }
