aa13b535084492509a46de22f47c9757d1c5abdf
whq <dawangong@126.com>
2019-01-17 17:42:58 +0800

    加入mobx

--- react-demo-build/src/components/App.js
+++ react-demo-build/src/components/App.js
@@ -2,19 +2,56 @@ import React, { Component } from 'react';
 import LikeButton from './button';
 import Header from './header';
 import List from './list';
+import Search from './search';
 
 export default class App extends Component {
     constructor(props) {
         super(props);
+
+        const list = [
+            {
+                title: 'React',
+                url: 'https://facebook.github.io/react/',
+                author: 'Jordan Walke',
+                num_comments: 3,
+                points: 4,
+                objectID: 0,
+            },
+            {
+                title: 'Redux',
+                url: 'https://github.com/reactjs/redux',
+                author: 'Dan Abramov, Andrew Clark',
+                num_comments: 2,
+                points: 5,
+                objectID: 1,
+            },
+        ];
+
+        this.state = {
+            list,
+            searchTerm: ''
+        };
+    }
+
+    onSearchChange(ev) {
+        this.setState({ searchTerm: ev.target.value });
     }
 
     render() {
+        const { searchTerm, list } = this.state;
         return (
             <div className="main">
                 主页
                 <Header />
                 <LikeButton />
-                <List />
+                <Search
+                    value={searchTerm}
+                    onChange={() => this.onSearchChange(event)}
+                />
+                <List
+                    list={list}
+                    pattern={searchTerm}
+                />
             </div>
         );
     }
--- react-demo-build/src/components/list.js
+++ react-demo-build/src/components/list.js
@@ -1,37 +1,36 @@
 import React, { Component }  from 'react';
+import {observable} from 'mobx';
+import {observer} from 'mobx-react';
 
 export default class List extends Component {
+
     constructor(props) {
         super(props);
-        this.list = [
-            {
-                title: 'React',
-                url: 'https://facebook.github.io/react/',
-                author: 'Jordan Walke',
-                num_comments: 3,
-                points: 4,
-                objectID: 0,
-            },
-            {
-                title: 'Redux',
-                url: 'https://github.com/reactjs/redux',
-                author: 'Dan Abramov, Andrew Clark',
-                num_comments: 2,
-                points: 5,
-                objectID: 1,
-            },
-        ];
+    }
+
+    isSearched(searchTerm) {
+        return item => item.title.toLowerCase().includes(searchTerm.toLowerCase());
+    }
+
+    onDismiss(id) {
+        const { list } = this.props;
+        const updatedList = list.filter(item => item.objectID !== id);
+        this.setState({ list: updatedList });
     }
 
     render() {
+        const { list, pattern } = this.props;
         return (
             <div>
-                {this.list.map(item =>
+                {list.filter(this.isSearched(pattern)).map(item =>
                     <div key={item.objectID}>
                         <p><a href={item.url}>{item.title}</a></p>
                         <p>{item.author}</p>
                         <p>{item.num_comments}</p>
                         <p>{item.points}</p>
+                        <button
+                            onClick={() => this.onDismiss(item.objectID)}
+                        > Dismiss</button>
                     </div>)}
             </div>
         );
