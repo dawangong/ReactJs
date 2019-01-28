60a737195cc70f20e5e1760d0746c57fc9f276ae
whq <dawangong@126.com>
2019-01-28 14:47:07 +0800

    game update

--- react-game/src/components/Game.js
+++ react-game/src/components/Game.js
@@ -1,16 +1,59 @@
 import React, { Component } from 'react';
 import Board from './Board';
+import calculateWinner from '../common/utils/winner'
 
 export default class Game extends Component {
 
+    constructor() {
+        super();
+        this.state = {
+            history: [{
+                squares: new Array(9).fill(null),
+            }],
+            xIsNext: true,
+        };
+    }
+
+    handleClick(i) {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+
+        this.setState({
+            history: [...history, ...[{
+                squares,
+            }]],
+            xIsNext: !this.state.xIsNext,
+        });
+    }
+
     render() {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const winner = calculateWinner(current.squares);
+
+        let status;
+        if (winner) {
+            status = `获胜者是: ${winner}`;
+        } else {
+            status = `轮到玩家: ${this.state.xIsNext ? 'X' : 'O'}`;
+        }
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board
+                        squares={current.squares}
+                        onClick={(i) => this.handleClick(i)}
+                    />
                 </div>
                 <div className="game-info">
-                    <div>{/* status */}</div>
+                    <div>{ status }</div>
                     <ol>{/* TODO */}</ol>
                 </div>
             </div>
