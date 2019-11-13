db295aad7e141ecd32de8ca8f099df435edb2a2d
whq <dawangong@126.com>
2019-11-13 16:11:52 +0800

    init and learn router demo how to use

--- react-web/src/index.js
+++ react-web/src/index.js
@@ -4,10 +4,15 @@ import ReactDOM from 'react-dom';
 import routerConfig from './router/index';
 import * as serviceWorker from './serviceWorker';
 import { renderRoutes } from 'react-router-config';
-import { HashRouter as Router } from 'react-router-dom';
+import { HashRouter as Router, Link } from 'react-router-dom';
 
 ReactDOM.render(
   <Router>
+    <ul>
+      <li><Link to="/">app</Link></li>
+      <li><Link to="/test/1">test</Link></li>
+      <li><Link to="/404">404</Link></li>
+    </ul>
     {renderRoutes(routerConfig)}
   </Router>,
   document.getElementById('root'));
