42107b67e80c8ebcfe2c06ac41fc55383a6ed9a2
whq <dawangong@126.com>
2020-01-03 11:12:50 +0800

    local proxy

--- react-front-end/src/setupProxy.js
+++ react-front-end/src/setupProxy.js
@@ -2,6 +2,13 @@ const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
   app.use(
+    proxy("/api/other", {
+      target: "https://api.i-meto.com",
+      changeOrigin: true,
+      pathRewrite: {"^/api/other" : "/"}
+    })
+  );
+  app.use(
     proxy("/api", {
       target: "http://localhost:3000",
       changeOrigin: true
